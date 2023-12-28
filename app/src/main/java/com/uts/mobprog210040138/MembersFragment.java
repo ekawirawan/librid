cb2e244704ae6273a35c3bdb28c956e04c246be0
artesia0528 <120473425+artesia0528@users.noreply.github.com>
2023-12-28 17:34:54 +0800

    memberrr uppdaatee

--- app/src/main/java/com/uts/mobprog210040138/MembersFragment.java
+++ app/src/main/java/com/uts/mobprog210040138/MembersFragment.java
@@ -1,12 +1,25 @@
 package com.uts.mobprog210040138;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.uts.mobprog210040138.models.ModelAPIResMember;
+import com.uts.mobprog210040138.models.ModelMember;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +27,14 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MembersFragment extends Fragment {
+
+    Context ctx;
+    RecyclerView recyclerView1;
+    ModelAPIResMember result;
+    List<ModelMember> data1;
+    RecyclerViewCustomAdapterMembers adapterMember;
+    private View view;
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +74,53 @@ public class MembersFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        ctx = getContext();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_members, container, false);
+        view = inflater.inflate(R.layout.fragment_members, container, false);
+        recyclerView1 = view.findViewById(R.id.recyclerViewMember);
+
+        LinearLayoutManager manager = new LinearLayoutManager(ctx);
+        recyclerView1.setLayoutManager(manager);
+        recyclerView1.setHasFixedSize(true);
+
+        loadDataMember();
+
+        return view;
+        //return inflater.inflate(R.layout.fragment_members, container, false);
+    }
+
+    public void loadDataMember() {
+        APIInterfaceMembers apiService;
+        apiService = APIClient.getClient().create(APIInterfaceMembers.class);
+        Call<ModelAPIResMember> getAllMember = apiService.getAllMember();
+
+        getAllMember.enqueue(new Callback<ModelAPIResMember>() {
+            @Override
+            public void onResponse(Call<ModelAPIResMember> call, Response<ModelAPIResMember> response) {
+                if (response.code() !=200){
+                    Toast.makeText(getContext(), "code" + response.code(),
+                            Toast.LENGTH_LONG).show();
+                }else{
+                    if (response.body() == null){
+
+                    }else {
+                        result = response.body();
+                        data1 = result.getData();
+
+                        adapterMember = new RecyclerViewCustomAdapterMembers(ctx, data1);
+                        recyclerView1.setAdapter(adapterMember);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ModelAPIResMember> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/uts/mobprog210040138/RecyclerViewCustomAdapterMembers.java
+++ app/src/main/java/com/uts/mobprog210040138/RecyclerViewCustomAdapterMembers.java
@@ -1,5 +1,69 @@
 package com.uts.mobprog210040138;
 
 
-public class RecyclerViewCustomAdapterMembers {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.uts.mobprog210040138.models.ModelMember;
+
+
+public class RecyclerViewCustomAdapterMembers extends RecyclerView.Adapter<RecyclerViewCustomAdapterMembers.ViewHolder> {
+    Context ctx;
+
+    private static ClickListener clickListener;
+
+    List<ModelMember> data;
+
+    public RecyclerViewCustomAdapterMembers(Context context, List<ModelMember> dataMember) {
+        ctx = context;
+        data = dataMember;
+    }
+
+    public void setOnItemCLickListener(ClickListener clickListener){
+        RecyclerViewCustomAdapterMembers.clickListener = clickListener;
+    }
+
+    public interface ClickListener {
+        void onItemClick(int position, View view);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView txtUsername, txtFullName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtUsername = itemView.findViewById(R.id.txtUsername);
+            txtFullName = itemView.findViewById(R.id.txtFullName);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) { clickListener.onItemClick(getAdapterPosition(), view); }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewCustomAdapterMembers.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.list_data_member, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewCustomAdapterMembers.ViewHolder holder, int position) {
+        ModelMember member = data.get(position);
+        holder.txtUsername.setText(member.getUsername());
+        holder.txtFullName.setText(member.getFullName());
+    }
+    @Override
+    public int getItemCount() { return data.size(); }
+
 }
